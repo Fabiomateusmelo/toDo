99ad9d8b3083a407c8a59cd64f70c99704b597fe
Fábio Mateus Melo <fabioceto@gmail.com>
2022-10-04 17:58:38 -0300

    0004

--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -2,6 +2,8 @@ import styles from './task.module.css'
 import { TbTrash, TbEdit } from 'react-icons/tb'
 import { ITask } from '../../App'
 import { BsFillCheckCircleFill } from 'react-icons/bs'
+import { EditText, EditTextarea } from 'react-edit-text';
+import { Tasks } from '../Tasks';
 
 interface Props {
   task: ITask;
@@ -17,7 +19,10 @@ export function Task({task, onDelete, onComplete}: Props) {
         {task.completed ? <BsFillCheckCircleFill/> : <div />}
       </button>
       <p className={task.completed ? styles.textCompleted : ""}>
-        {task.title}
+      <EditText
+              defaultValue={(task.title)}
+              style={{fontSize: '16px', backgroundColor: "#262626", color:'#f2f2f2', border:'0', borderRadius:'0.2rem', minWidth:'300px'}}
+            />
       </p>
       <button className={styles.delete} onClick={() => onDelete(task.id)}>
         <TbTrash size={20} />
